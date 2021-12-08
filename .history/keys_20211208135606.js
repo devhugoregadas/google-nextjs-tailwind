bbeabd2eb877c7f57c16a58bafe825ce67c2c1ef
Hugo <dev.hugoregadas@gmail.com>
2021-12-08 13:57:53 +0100

    keys

--- .history/keys_20211208135606.js
+++ .history/keys_20211208135606.js
@@ -2,4 +2,4 @@
 export const API_KEY = "<AIzaSyCcJArQjZ-hFxpTD7_m6rrhw_v-HrmQyk8>";
 
 // https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "c5faaf8122c7d45ad"; // << Replace with your own CX key
+export const CONTEXT_KEY = "9867c733a42c0c643"; // << Replace with your own CX key
